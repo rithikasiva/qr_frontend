5a2c919d1925e8a596e9f59ba78c14027884a4fd
rithikasiva <87708931+rithikasiva@users.noreply.github.com>
2022-05-15 15:05:29 +0530

    Update getin.js

--- getin.js
+++ getin.js
@@ -5,4 +5,16 @@ console.log(params);
 
 let tokenText = document.querySelector(".token");
 
-tokenText.innerHTML = params.token;
+fetch("http://localhost:8080/check", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ token: params.token }),
+})
+  .then((res) => res.json())
+  .then((res) => {
+    if (res.status === "success") {
+      tokenText.innerHTML = params.token;
+    } else {
+      tokenText.innerHTML = "Invalid Token";
+    }
+  });
